28007772bad0b6f3371a98e954f1e62e49528db1
Octavian <sopronoctavian@gmail.com>
2019-07-20 14:19:35 +0300

    Started style for waiting players

--- src/pages/GameLobbyPage/WaitPlayersPage/WaitPlayersPage.jsx
+++ src/pages/GameLobbyPage/WaitPlayersPage/WaitPlayersPage.jsx
@@ -1,12 +1,16 @@
-import React, { Component } from "react";
-import { databaseRefs } from "../../../lib/refs";
-import { getToupleFromSnapshot } from "../../../lib/firebaseUtils";
+import React, { Component } from 'react';
+import { databaseRefs } from '../../../lib/refs';
+import { getToupleFromSnapshot } from '../../../lib/firebaseUtils';
+
+import './WaitPlayersPage.scss';
+
+import { Card, Animal } from '../../../shared';
 
 class WaitPlayersPage extends Component {
   state = {
     limit: 0,
     players: [],
-    currentScreen: ""
+    currentScreen: ''
   };
 
   gameRef;
@@ -18,25 +22,29 @@ class WaitPlayersPage extends Component {
         params: { gameId }
       }
     } = this.props;
-    const playerInfo = JSON.parse(localStorage.getItem("playerInfo"));
+    const playerInfo = JSON.parse(localStorage.getItem('playerInfo'));
     const { playerId } = playerInfo;
     this.gameRef = databaseRefs.game(gameId);
     this.playerRef = databaseRefs.player(gameId, playerId);
 
-    this.playerRef.child("/isReady").set(true);
+    this.playerRef.child('/isReady').set(true);
 
-    this.gameRef.on("value", snapshot => {
+    this.gameRef.on('value', snapshot => {
       const { players } = snapshot.val();
       this.setState({ players: getToupleFromSnapshot(players) });
     });
 
-    this.gameRef.child("/currentScreen").on("value", snapshot => {
+    this.gameRef.child('/currentScreen').on('value', snapshot => {
       const { history } = this.props;
       if (snapshot.val()) {
         const { route } = snapshot.val();
         history.push(route);
       }
     });
+  }
+
+  componentDidUpdate() {
+    console.log('update', this.state);
   }
 
   setPlayerNotReady = () => {
@@ -46,10 +54,10 @@ class WaitPlayersPage extends Component {
       }
     } = this.props;
 
-    const playerInfo = JSON.parse(localStorage.getItem("playerInfo"));
+    const playerInfo = JSON.parse(localStorage.getItem('playerInfo'));
     const { playerId } = playerInfo;
     this.playerRef = databaseRefs.player(gameId, playerId);
-    this.playerRef.child("/isReady").set(false);
+    this.playerRef.child('/isReady').set(false);
   };
 
   componentWillUnmount() {
@@ -62,15 +70,25 @@ class WaitPlayersPage extends Component {
     return players.map(player => {
       const [key, data] = player;
       return (
-        <li key={key}>
-          <span>{data.nickname} - ready</span>
-        </li>
+        <div
+          key={key}
+          className="team o-layout--stretch u-padding-small u-margin-bottom-small"
+        >
+          <Animal className="u-margin-right-small" />
+          <Card className="player u-margin-vertical-small u-weight-bold u-2/3">
+            {data.isReady ? (
+              data.nickname
+            ) : (
+              <span className="waiting">Loading...</span>
+            )}
+          </Card>
+        </div>
       );
     });
   };
 
   render() {
-    return <ul>{this.renderListOfPlayersReady()}</ul>;
+    return this.renderListOfPlayersReady();
   }
 }
 
