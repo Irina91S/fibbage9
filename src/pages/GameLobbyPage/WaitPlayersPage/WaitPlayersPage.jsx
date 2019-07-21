1cb5b1f930c41dc34acea329e655ef9e4470aee4
Silvestru Irina <i.silvestru@levi9.com>
2019-07-21 13:03:22 +0300

    Checkpoint: * add Header to wating players page

--- src/pages/GameLobbyPage/WaitPlayersPage/WaitPlayersPage.jsx
+++ src/pages/GameLobbyPage/WaitPlayersPage/WaitPlayersPage.jsx
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
-import { databaseRefs } from '../../../lib/refs';
-import { getToupleFromSnapshot } from '../../../lib/firebaseUtils';
+import React, { Component, Fragment } from "react";
+import { databaseRefs } from "../../../lib/refs";
+import { getToupleFromSnapshot } from "../../../lib/firebaseUtils";
 
-import './WaitPlayersPage.scss';
+import "./WaitPlayersPage.scss";
 
-import { Card, Animal } from '../../../shared';
+import { Card, Animal } from "../../../shared";
 
 class WaitPlayersPage extends Component {
   state = {
     limit: 0,
     players: [],
-    currentScreen: ''
+    currentScreen: ""
   };
 
   gameRef;
@@ -22,17 +22,21 @@ class WaitPlayersPage extends Component {
         params: { gameId }
       }
     } = this.props;
-    const playerInfo = JSON.parse(localStorage.getItem('playerInfo'));
+    const playerInfo = JSON.parse(localStorage.getItem("playerInfo"));
     const { playerId } = playerInfo;
     this.gameRef = databaseRefs.game(gameId);
     this.playerRef = databaseRefs.player(gameId, playerId);
 
-    this.gameRef.on('value', snapshot => {
-      const { players } = snapshot.val();
-      this.setState({ players: getToupleFromSnapshot(players) });
+    this.gameRef.on("value", snapshot => {
+      const { players, limit } = snapshot.val();
+
+      this.setState({
+        players: getToupleFromSnapshot(players),
+        limit
+      });
     });
 
-    this.gameRef.child('/currentScreen').on('value', snapshot => {
+    this.gameRef.child("/currentScreen").on("value", snapshot => {
       const { history } = this.props;
       if (snapshot.val()) {
         const { route } = snapshot.val();
@@ -42,7 +46,7 @@ class WaitPlayersPage extends Component {
   }
 
   componentDidUpdate() {
-    console.log('update', this.state);
+    console.log("update", this.state);
   }
 
   setPlayerNotReady = () => {
@@ -52,7 +56,7 @@ class WaitPlayersPage extends Component {
       }
     } = this.props;
 
-    const playerInfo = JSON.parse(localStorage.getItem('playerInfo'));
+    const playerInfo = JSON.parse(localStorage.getItem("playerInfo"));
     const { playerId } = playerInfo;
     this.playerRef = databaseRefs.player(gameId, playerId);
   };
@@ -71,24 +75,53 @@ class WaitPlayersPage extends Component {
       const [key, data] = player;
       const style = { color: data.animal.color };
       return (
-        <div
-          key={key}
-          className="team o-layout--stretch u-padding-small u-margin-bottom-small"
-        >
-          <Animal className="u-margin-right-small" animal={data.animal.animal}/>
-          <Card
-            className="player u-margin-vertical-small u-weight-bold u-2/3"
-            style={style}
-          >
-            {data.nickname}
-          </Card>
+        <div key={key} className="team  u-padding-small u-margin-bottom-small">
+          <div className="o-layout--flex">
+            <Animal
+              className="u-margin-right-small"
+              animal={data.animal.animal}
+            />
+            <Card
+              className="player u-margin-vertical-small u-weight-bold u-2/3"
+              style={style}
+            >
+              {data.nickname}
+            </Card>
+          </div>
         </div>
       );
     });
   };
 
+  renderListOfReadyAnimals = () => {
+    const { players } = this.state;
+    return players.map(player => {
+      const [key, data] = player;
+      return (
+        <Animal
+          key={key}
+          className="u-margin-right-small"
+          animal={data.animal.animal}
+        />
+      );
+    });
+  };
+
   render() {
-    return this.renderListOfPlayersReady();
+    const { limit, players } = this.state;
+
+    return (
+      <Fragment>
+        <div>
+          <h3>Waiting for all teams</h3>
+          <div className="o-layout--flex">
+            {`${players.length}/${limit} Teams are ready`}{" "}
+            {this.renderListOfReadyAnimals()}
+          </div>
+        </div>
+        {this.renderListOfPlayersReady()}
+      </Fragment>
+    );
   }
 }
 
