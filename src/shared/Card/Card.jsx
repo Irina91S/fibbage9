cb44abb16c6749da226af1b1df511e5bbcb00a1d
melluz <silviu.madalin.m32@gmail.com>
2019-07-21 17:45:27 +0300

    chore: various design shit

--- src/shared/Card/Card.jsx
+++ src/shared/Card/Card.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-
 import './Card.scss';
 
 // basic or success cards
-const Card = ({ className, type = 'basic', children, ...rest }) => {
+const Card = ({ className, disabled, type = 'basic', children, ...rest }) => {
   return (
-    <div
-      className={`card o-box--small ${type} ${className ? className : ''}`}
-      {...rest}
-    >
-      {children}
-    </div>
+    <React.Fragment>
+      {console.log(disabled)}
+      <div
+        className={`card o-box--small ${type} ${disabled ? 'disabled' : ''}  ${className ? className : ''}`}
+        {...rest}
+      >
+        {children}
+      </div>
+    </React.Fragment> 
   );
 };
 
