5fa381207893b6689d5b4f7545dd22f03f661322
melluz <silviu.madalin.m32@gmail.com>
2019-07-22 22:04:15 +0300

    feature: added total score page

--- src/pages/GameLobbyPage/TotalScoresPage/TotalScoresPage.jsx
+++ src/pages/GameLobbyPage/TotalScoresPage/TotalScoresPage.jsx
@@ -1,8 +1,18 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import anime from 'animejs';
 import { databaseRefs } from "../../../lib/refs";
 import { getToupleFromSnapshot } from "../../../lib/firebaseUtils";
+import Animal from '../../../shared/Animal/Animal';
+import Card from '../../../shared/Card/Card';
+
+import FirstPlaceCrown from '../../../shared/assets/svg/first-place.svg';
+import SecondPlaceCrown from '../../../shared/assets/svg/second-place.svg';
+
+import './TotalScoresPage.scss';
 
 const { players } = databaseRefs;
+
+const dimensions = { height: 72, width: 72 };
 
 class TotalScorePage extends Component {
   state = {
@@ -16,6 +26,100 @@ class TotalScorePage extends Component {
 
   sortPlayersByScore = players =>
     players.sort((player1, player2) => player2.totalScore - player1.totalScore);
+
+  renderListOfPlayers = () => {
+    const { players } = this.state;
+    const playerInfo = JSON.parse(localStorage.getItem('playerInfo'));
+    const { playerName } = playerInfo;
+
+    if (!players || players.length === 0) {
+      return [];
+    }
+
+    const playersSorted = players
+      .sort((player1, player2) => player2.totalScore - player1.totalScore);
+
+    console.log(playersSorted);
+
+    return players
+      .sort((player1, player2) => player2.totalScore - player1.totalScore)
+      .map((player, index) => {
+        const isCurrentPlayer = playerName === player.nickname;
+        const color = player.animal ? player.animal.color : '';
+
+        const style = {
+          color: color,
+          border: `2px solid ${color}`
+        };
+
+        const bgStyle = {
+          position: 'absolute',
+          left: '0',
+          top: '0',
+          backgroundColor: color,
+          opacity: '.3',
+          width: '100%',
+          height: '100%'
+        };
+
+        console.log(bgStyle);
+
+        return (
+          <Fragment key={player.animal.animal}>
+            {index <= 1 &&
+              <div className="winner o-layout--center o-layout--stretch u-1/1">
+                {index === 0 && <img src={FirstPlaceCrown} alt="first-place" />}
+                {index === 1 && <img src={SecondPlaceCrown} alt="second-place" />}
+
+                <Card className="u-1/1 u-margin-bottom-small u-1/1 u-weight-bold card cancer" style={style}>
+                  {isCurrentPlayer && <div style={{...bgStyle, 'backgroundColor': color}} />}
+                  <div className="card--left">
+                    <div className="u-h4 u-margin-bottom-small">
+                      {player.nickname}
+                    </div>
+                    <div className="u-h5">
+                      SCORE: {player.totalScore}
+                    </div>
+                  </div>
+
+                  <div className="card--right">
+                    <Animal
+                      className="u-margin-right-small"
+                      style={dimensions}
+                      animal={player.animal.animal}
+                    />
+                  </div>
+                </Card>
+
+                {index === 1 && <h1 className="u-h4 u-margin-top-small u-color-main">Better luck next time</h1>}
+              </div>
+            }
+            {
+              index > 1 &&
+              <div className="grid-item loser">
+                {isCurrentPlayer && console.log(color)}
+                <Card className="u-1/1 u-margin-bottom-small cancer-container" style={{...style, padding: '6px'}}>
+                {isCurrentPlayer && <div style={{...bgStyle, 'backgroundColor': color}}/>}
+                  <div className="u-h6 u-margin-bottom-small">
+                    {player.nickname}
+                  </div>
+
+                  <Animal
+                    className="u-margin-bottom-small"
+                    style={dimensions}
+                    animal={player.animal.animal}
+                  />
+
+                  <div className="u-h5">
+                    SCORE: {player.totalScore}
+                  </div>
+                </Card>
+              </div>
+            }
+          </Fragment>
+        );
+      });
+  };
 
   componentDidMount() {
     const {
@@ -31,7 +135,26 @@ class TotalScorePage extends Component {
         getToupleFromSnapshot(playersSnapshot)
       );
       const sortedPlayers = this.sortPlayersByScore(playersInfo);
-      this.setState({ players: playersInfo, sortedPlayers });
+      this.setState({ players: playersInfo, sortedPlayers }, () => {
+        anime({
+          targets: '.winner',
+          translateX: [-1000, 0],
+          opacity: [0, 1],
+          delay: anime.stagger(100),
+          easing: 'easeInOutQuint',
+          duration: 400,
+        });
+
+        anime({
+          display: 'inline-flex',
+          targets: '.loser',
+          translateY: [30, 0],
+          opacity: [0, 1],
+          delay: anime.stagger(100),
+          easing: 'easeInOutQuint',
+          duration: 800
+        });
+      });
     });
   }
 
@@ -45,12 +168,7 @@ class TotalScorePage extends Component {
     const { sortedPlayers } = this.state;
     return (
       <div>
-        {sortedPlayers.map((player, index) => (
-          <div key={`${player.nickname}${index}`}>
-            <div>{player.nickname}</div>
-            <div>{player.totalScore}</div>
-          </div>
-        ))}
+        {this.renderListOfPlayers()}
       </div>
     );
   }
