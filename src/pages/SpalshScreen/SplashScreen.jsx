f4db76e8ac350ada370851f17e765c7bffe1992e
Octavian <sopronoctavian@gmail.com>
2019-07-23 00:12:47 +0300

    Fixed splashscreen

--- src/pages/SpalshScreen/SplashScreen.jsx
+++ src/pages/SpalshScreen/SplashScreen.jsx
@@ -1,55 +1,76 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
 import anime from 'animejs';
 
-import "./SplashScreen.scss";
+import './SplashScreen.scss';
 
 class SplashScreen extends React.Component {
   componentDidMount() {
-    anime.timeline({ loop: true }).add({
-      targets: '.letter',
-      translateX: [40, 0],
-      translateZ: 0,
-      opacity: [0, 1],
-      easing: "easeOutExpo",
-      duration: 1200,
-      delay: function (el, i) {
-        return 500 + 30 * i;
-      }
-    }).add({
-      targets: '.letter',
-      translateX: [0, -30],
-      opacity: [1, 0],
-      easing: "easeInExpo",
-      duration: 3000,
-      delay: function (el, i) {
-        return 100 + 30 * i;
-      }
-    });
+    setTimeout(() => {
+      anime
+        .timeline({ loop: false })
+        .add({
+          targets: '.container',
+          height: [0, '100vh'],
+          easing: 'easeOutExpo',
+          duration: 1200
+        })
+        .add({
+          targets: '.info',
+          translateY: [30, 0],
+          opacity: [0, 1],
+          easing: 'easeOutExpo',
+          duration: 1500
+        })
+        .add({
+          targets: '.letter',
+          translateX: [40, 0],
+          opacity: [0, 1],
+          easing: 'easeOutExpo',
+          duration: 1300,
+          delay: function(el, i) {
+            return 400 + 30 * i;
+          }
+        })
+        .add({
+          targets: '.letter',
+          translateX: [0, -30],
+          opacity: [1, 0],
+          easing: 'easeInExpo',
+          duration: 800,
+          delay: function(el, i) {
+            return 100 + 30 * i;
+          }
+        })
+        .add({
+          targets: '.container',
+          opacity: [1, 0],
+          easing: 'easeOutExpo',
+          duration: 1500
+        }).complete = () => {
+        this.props.onComplete();
+      };
+    }, 100);
   }
 
-  renderLetter = (text = 'Levi9 edition') => (
-    text.split('').map((letter) => (
+  renderLetter = (text = 'Levi9 edition') =>
+    text.split('').map(letter => (
       <span
-        key={letter + (100 * Math.random())}
+        key={letter + 100 * Math.random()}
         style={{ width: letter.trim() ? '' : '4px' }}
-        className="letter ">{letter}
+        className="letter "
+      >
+        {letter}
       </span>
-    ))
-  );
+    ));
 
   render() {
     return (
-      <div className="container">
-        <div className="branding u-h1 u-margin-bottom">
-          Fibbage
-            </div>
-        <div className="infoo u-h3">
-          {this.renderLetter()}
-        </div>
+      <div className="splash-screen container">
+        <div className="branding u-h1 u-margin-bottom">Fibbage</div>
+        <div className="infoo u-h3">{this.renderLetter()}</div>
       </div>
     );
   }
-
-};
+}
 
 export default SplashScreen;
