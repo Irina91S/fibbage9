68fcda8560429a16148c1783c58b48e96d09e80f
Silvestru Irina <i.silvestru@levi9.com>
2019-07-23 20:46:09 +0300

    add nose for owl svg

--- src/pages/StartPage/Start.jsx
+++ src/pages/StartPage/Start.jsx
@@ -3,7 +3,6 @@ import anime from 'animejs';
 import { databaseRefs } from './../../lib/refs';
 import { getToupleFromSnapshot } from './../../lib/firebaseUtils';
 import { InsertPincodeForm, Rocket, Moon } from './components';
-
 const { games } = databaseRefs;
 
 class Start extends React.Component {
