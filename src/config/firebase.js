f382ba0fee92552698ab3659bc6ac383b4ff5be4
oct4vian <sopronoctavian@gmail.com>
2019-07-25 12:18:04 +0300

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyAyhJbLtU1xeAmQfpu-8mH0vzP9u9yjgq0",
-  authDomain: "fibbage-31361.firebaseapp.com",
-  databaseURL: "https://fibbage-31361.firebaseio.com",
-  projectId: "fibbage-31361",
-  storageBucket: "fibbage-31361.appspot.com",
-  messagingSenderId: "431881259194",
-  appId: "1:431881259194:web:42ccd03f89329b6b"
+  apiKey: 'AIzaSyArJvuM9ClAN-fUW74Bcelo6mGZdosiAZQ',
+  authDomain: 'fibbage2.firebaseapp.com',
+  databaseURL: 'https://fibbage2.firebaseio.com',
+  projectId: 'fibbage2',
+  storageBucket: 'fibbage2.appspot.com',
+  messagingSenderId: '529443257048',
+  appId: '1:529443257048:web:1d9287fc6836df0f'
 };
 
 firebase.initializeApp(config);
