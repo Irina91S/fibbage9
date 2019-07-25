82e49c61759bb4da905c01878970195059f4e4d8
Adelina Dascalu <a.dascalu@levi9.com>
2019-07-25 12:03:06 +0300

    final questions

--- cloud-functions/functions/questionsList.js
+++ cloud-functions/functions/questionsList.js
@@ -36,7 +36,7 @@ const questionsList = [
       value: 'fridge',
     },
     index: 4,
-    question: '29th May is officially “Put a Pillow on Your ........ Day',
+    question: '29th May is officially “Put a pillow on your ........ day',
     score: 100
   },
   {
@@ -46,6 +46,158 @@ const questionsList = [
     index: 5,
     question: 'Heart attacks are more likely to happen on .......',
     score: 100
+  },
+  {
+    answer: {
+      value: 'fall asleep',
+    },
+    index: 6,
+    question: 'When Ancuta watches interesting crime police work related movies, she automatically  .......',
+    score: 100
+  },
+  {
+    answer: {
+      value: 'shark attacks',
+    },
+    index: 7,
+    question: 'In 2017 more people were killed from injuries caused by taking a selfie than by .......',
+    score: 100
+  },
+  {
+    answer: {
+      value: 'hot air balloon',
+    },
+    index: 8,
+    question: 'A sheep, a duck and a rooster were the first passengers in a .......',
+    score: 100
+  },
+  {
+    answer: {
+      value: 'penguin urine',
+    },
+    index: 9,
+    question: 'Nearly 3% of the ice in Antarctic glaciers is  .......',
+    score: 100
+  },
+  {
+    answer: {
+      value: 'Larry',
+    },
+    index: 10,
+    question: 'The Twitter bird actually has a name: .......',
+    score: 100
+  },
+  {
+    answer: {
+      value: 'eight',
+    },
+    index: 11,
+    question: 'Vikingu (aka Irina) had ....... colors in her hair this summer.',
+    score: 200
+  },
+  {
+    answer: {
+      value: 'Dying',
+    },
+    index: 12,
+    question: '....... is illegal in the Houses of Parliaments',
+    score: 200
+  },
+  {
+    answer: {
+      value: 'Facebook',
+    },
+    index: 13,
+    question: 'In 2011, more than 1 in 3 divorce filings in the U.S. contained the word: .......',
+    score: 100
+  },
+  {
+    answer: {
+      value: 'Mizerie',
+    },
+    index: 14,
+    question: '....... is the word Silviu is most inclined to say when you show him something.',
+    score: 200
+  },
+  {
+    answer: {
+      value: 'cheerleader ',
+    },
+    index: 15,
+    question: '17.	George W. Bush was a ......... in highschool.',
+    score: 200
+  },
+  {
+    answer: {
+      value: 'cat',
+    },
+    index: 16,
+    question: 'Ștefi owns only one ....... but she defineteley would like to have more.',
+    score: 200
+  },
+  {
+    answer: {
+      value: 'bacon',
+    },
+    index: 17,
+    question: 'Coca-Cola released a ‘Diet Coke with ...... flavor.',
+    score: 200
+  },
+  {
+    answer: {
+      value: 'sausages',
+    },
+    index: 18,
+    question: 'Car manufacturer Volkswagen makes .......',
+    score: 200
+  },
+  {
+    answer: {
+      value: 'desks',
+    },
+    index: 19,
+    question: 'Adelina has changed five different ........ in the last year',
+    score: 200
+  },
+  {
+    answer: {
+      value: 'jail',
+    },
+    index: 20,
+    question: 'Canada has the world’s smallest  .......',
+    score: 300
+  },
+  {
+    answer: {
+      value: 'onion',
+    },
+    index: 21,
+    question: 'An apple, potato, and ........ all taste the same if you eat them with your nose plugged',
+    score: 300
+  },
+  {
+    answer: {
+      value: 'makes memes',
+    },
+    index: 22,
+    question: 'At work, Octavian usually ........',
+    score: 300
+  },
+  {
+    answer: {
+      value: 'burning',
+    },
+    index: 23,
+    question: 'The 20th of March is Snowman ........ Day.',
+    score: 300
+  },
+  {
+    answer: {
+      value: 'coffee',
+    },
+    index: 24,
+    question: 'In the 16th Century, Arab women could initiate a divorce if their husbands didn’t pour ....... for them',
+    score: 300
   }
 ];
 
